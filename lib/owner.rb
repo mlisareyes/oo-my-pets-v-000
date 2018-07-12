2301d1d9ea3d264177c952316d7d9f0bb52a8105
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-12 03:51:03 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,19 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    self.pets = {:fishes=>[], :cats=>[], :dogs =>[]}
+  end
+
+  def self.reset_all
+  end
+
+  def self.all
+    @@all
+  end
 end
